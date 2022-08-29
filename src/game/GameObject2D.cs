c4a8f2c45729d403e2096ea54c294780b227033f
Astral-Sheep <109028693+Astral-Sheep@users.noreply.github.com>
2022-08-29 21:12:12 +0200

    feat: add class GameObject2D

---
+++ src/game/GameObject2D.cs
@@ -0,0 +1,38 @@
+﻿using Godot;
+using System;
+
+namespace Com.Surbon.GDUtils.Game
+{
+	public class GameObject2D : Node2D, IStateMachine
+	{
+		protected Action<float> DoAction;
+
+		public override void _Ready()
+		{
+			base._Ready();
+
+			SetModeVoid();
+		}
+
+		public override void _Process(float pDelta)
+		{
+			base._Process(pDelta);
+
+			DoAction(pDelta);
+		}
+
+		public void Start() { SetModeNormal(); }
+
+		void IStateMachine.SetModeVoid() { SetModeVoid(); }
+		protected void SetModeVoid() { DoAction = DoActionVoid; }
+
+		void IStateMachine.SetModeNormal() { SetModeNormal(); }
+		protected void SetModeNormal() { DoAction = DoActionNormal; }
+
+		void IStateMachine.DoActionVoid(float pDelta) { DoActionVoid(pDelta); }
+		protected void DoActionVoid(float pDelta) { }
+
+		void IStateMachine.DoActionNormal(float pDelta) { DoActionNormal(pDelta); }
+		protected void DoActionNormal(float pDelta) { }
+	}
+}
