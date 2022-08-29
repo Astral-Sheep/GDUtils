e982bef15849a02214b2d37a4c29b88334162d5f
Astral-Sheep <109028693+Astral-Sheep@users.noreply.github.com>
2022-08-29 21:28:26 +0200

    feat: add classes Property and Node in class Property

---
+++ src/properties/Property.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Surbon.GDUtils.Properties
+{
+	public static class Property
+	{
+		public static class Node
+		{
+			public const string PAUSE_MODE = "pause_mode";
+			public const string PROCESS_PRIORITY = "process_priority";
+			public const string SCRIPT = "script";
+		}
+	}
+}
