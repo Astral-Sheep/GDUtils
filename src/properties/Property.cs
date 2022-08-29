c1533415ce9733d6e278cf58bf9e3f385c2edda2
Astral-Sheep <109028693+Astral-Sheep@users.noreply.github.com>
2022-08-29 21:39:43 +0200

    feat: add class Viewport in class Node

--- src/properties/Property.cs
+++ src/properties/Property.cs
@@ -12,6 +12,70 @@ namespace Com.Surbon.GDUtils.Properties
 			public const string PROCESS_PRIORITY = "process_priority";
 			public const string SCRIPT = "script";
 
+			public static class Viewport
+			{
+				public const string ARVR = "arvr";
+				public const string SIZE = "size";
+				public const string SIZE_OVERRIDE_STRETCH = "size_override_stretch";
+				public const string OWN_WORLD = "own_world";
+				public const string WORLD = "world";
+				public const string TRANSPARENT_BG = "transparent_bg";
+				public const string HANDLE_INPUT_LOCALLY = "handle_input_locally";
+
+				#region RENDERING
+
+				public const string MSAA = "msaa";
+				public const string FXAA = "fxaa";
+				public const string DEBANDING = "debanding";
+				public const string SHARPEN_INTENSITY = "sharpen_intensity";
+				public const string HDR = "hdr";
+				public const string DISABLE_3D = "disable_3d";
+				public const string KEEP_3D_LINEAR = "keep_3d_linear";
+				public const string USAGE = "usage";
+				public const string RENDER_DIRECT_TO_SCREEN = "render_direct_to_screen";
+				public const string DEBUG_DRAW = "debug_draw";
+
+				#endregion RENDERING
+
+				#region RENDER TARGET
+
+				public const string V_FLIP = "render_target_v_flip";
+				public const string CLEAR_MODE = "render_target_clear_mode";
+				public const string UPDATE_MODE = "render_target_update_mode";
+
+				#endregion RENDER TARGET
+
+				#region AUDIO LISTENER
+
+				public const string ENABLE_2D = "audio_listener_enable_2d";
+				public const string ENABLE_3D = "audio_listener_enable_3d";
+
+				#endregion AUDIO LISTENER
+
+				#region PHYSICS
+
+				public const string OBJECT_PICKING = "physics_object_picking";
+
+				#endregion PHYSICS
+
+				#region GUI
+
+				public const string DISABLE_INPUT = "gui_disable_input";
+				public const string SNAP_CONTROLS_TO_PIXELS = "gui_snap_controls_to_pixels";
+
+				#endregion GUI
+
+				#region SHADOW ATLAS
+
+				public const string SHADOW_ATLAS_SIZE = "shadow_atlas_size";
+				public const string QUAD_0 = "shadow_atlas_quad_0";
+				public const string QUAD_1 = "shadow_atlas_quad_1";
+				public const string QUAD_2 = "shadow_atlas_quad_2";
+				public const string QUAD_3 = "shadow_atlas_quad_3";
+
+				#endregion SHADOW ATLAS
+			}
+
 			public static class WorldEnvironment
 			{
 				public const string ENVIRONMENT = "environment";
