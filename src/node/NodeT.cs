6f7f159d6c5546264a6c39595b2c75701c452499
Astral-Sheep <109028693+Astral-Sheep@users.noreply.github.com>
2022-08-30 19:10:13 +0200

    feat: add methods SetProcess, DisableProcess and EnableProcess

--- src/node/NodeT.cs
+++ src/node/NodeT.cs
@@ -8,6 +8,22 @@ namespace Com.Surbon.GDUtils.Node
 	public static class NodeT
 	{
 		/// <summary>
+		/// Disable the process of the node and all its children.
+		/// </summary>
+		public static void DisableProcess(Godot.Node node)
+		{
+			SetProcess(node, false);
+		}
+
+		/// <summary>
+		/// Enable the process of the node and all its children.
+		/// </summary>
+		public static void EnableProcess(Godot.Node node)
+		{
+			SetProcess(node, true);
+		}
+
+		/// <summary>
 		/// Returns the nth parent of the given node, or a null instance if the node lacks parent. It returns the node's highest parent if it is below n.
 		/// </summary>
 		public static Godot.Node GetParent(Godot.Node node, int n)
@@ -47,6 +63,19 @@ namespace Com.Surbon.GDUtils.Node
 			}
 
 			return (T)lCurrentNode;
+		}
+
+		/// <summary>
+		/// Sets the process of the given node and all its children to the given boolean.
+		/// </summary>
+		private static void SetProcess(Godot.Node node, bool isProcessing)
+		{
+			node.SetProcess(isProcessing);
+
+			foreach (Godot.Node child in node.GetChildren())
+			{
+				SetProcess(child, isProcessing);
+			}
 		}
 	}
 }
