4b2e1edaeace8d7a6e2f669b356793ebd597f9ed
Astral-Sheep <109028693+Astral-Sheep@users.noreply.github.com>
2022-08-30 18:04:15 +0200

    feat: add class RandomT, and add methods Init, RandfExclusive and RandfInclusive in class RandomT

---
+++ src/math/RandomT.cs
@@ -0,0 +1,37 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Surbon.GDUtils.src.math
+{
+	public static class RandomT
+	{
+		private static RandomNumberGenerator inclusiveGenerator;
+		private static Random exclusiveGenerator;
+
+		public static void Init()
+		{
+			inclusiveGenerator = new RandomNumberGenerator();
+			inclusiveGenerator.Randomize();
+
+			exclusiveGenerator = new Random();
+		}
+
+		/// <summary>
+		/// Returns a random number between min and max ([min, max[).
+		/// </summary>
+		public static float RandfExclusive(float min = 0, float max = 1)
+		{
+			return min + (float)exclusiveGenerator.NextDouble() * (max - min);
+		}
+
+		/// <summary>
+		/// Returns a random number between min and max ([min, max]).
+		/// </summary>
+		public static float RandfInclusive(float min = 0, float max = 1)
+		{
+			return inclusiveGenerator.RandfRange(min, max);
+		}
+	}
+}
