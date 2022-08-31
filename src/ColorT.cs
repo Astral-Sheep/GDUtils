24281417b94e38db22ee60aad05713e4218c3ed0
Astral-Sheep <109028693+Astral-Sheep@users.noreply.github.com>
2022-08-31 16:48:21 +0200

    feat: add class ColorT

---
+++ src/ColorT.cs
@@ -0,0 +1,19 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Surbon.GDUtils
+{
+	public static class ColorT
+	{
+		public static readonly Color Black = new Color(0, 0, 0, 1);
+		public static readonly Color White = new Color(1, 1, 1, 1);
+		public static readonly Color Red = new Color(1, 0, 0, 1);
+		public static readonly Color Yellow = new Color(1, 1, 0, 1);
+		public static readonly Color Green = new Color(0, 1, 0, 1);
+		public static readonly Color Cyan = new Color(0, 1, 1, 1);
+		public static readonly Color Blue = new Color(0, 0, 1, 1);
+		public static readonly Color Pink = new Color(1, 0, 1, 1);
+	}
+}
