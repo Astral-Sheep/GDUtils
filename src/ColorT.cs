ca4db4e2eccc119d9db2ea83b7425aa70b5fd1a7
Astral-Sheep <109028693+Astral-Sheep@users.noreply.github.com>
2022-08-31 17:03:50 +0200

    feat: add methods GetGray and SetVisible in class ColorT

--- src/ColorT.cs
+++ src/ColorT.cs
@@ -1,12 +1,10 @@
 ﻿using Godot;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Com.Surbon.GDUtils
 {
 	public static class ColorT
 	{
+		public static readonly Color NONE = new Color(0, 0, 0, 0);
 		public static readonly Color Black = new Color(0, 0, 0, 1);
 		public static readonly Color White = new Color(1, 1, 1, 1);
 		public static readonly Color Red = new Color(1, 0, 0, 1);
@@ -15,6 +13,12 @@ namespace Com.Surbon.GDUtils
 		public static readonly Color Cyan = new Color(0, 1, 1, 1);
 		public static readonly Color Blue = new Color(0, 0, 1, 1);
 		public static readonly Color Pink = new Color(1, 0, 1, 1);
+
+		public static Color GetGray(Color color)
+		{
+			float gray = (color.r + color.g + color.b) / 3f;
+			return new Color(gray, gray, gray, color.a);
+		}
 
 		public static Color SetAlpha(Color color, float a)
 		{
@@ -34,6 +38,11 @@ namespace Com.Surbon.GDUtils
 		public static Color SetRed(Color color, float r)
 		{
 			return new Color(r, color.g, color.b, color.a);
+		}
+
+		public static Color SetVisible(Color color, bool isVisible)
+		{
+			return SetAlpha(color, isVisible ? 1f : 0f);
 		}
 	}
 }
