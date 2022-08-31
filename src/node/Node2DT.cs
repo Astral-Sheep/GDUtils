9128fd84409a609a7e96518720135494d8d787ba
Astral-Sheep <109028693+Astral-Sheep@users.noreply.github.com>
2022-08-31 12:31:12 +0200

    feat: add class Node2DT and add class AreaPlayer2D in class Node2DT

---
+++ src/node/Node2DT.cs
@@ -0,0 +1,24 @@
+﻿using Com.Surbon.GDUtils.StateMachines;
+using Godot;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Surbon.GDUtils.Node
+{
+	public static class Node2DT
+	{
+		public class AreaPlayer2D : GameObject2D
+		{
+			[Export] protected NodePath bodyPath;
+			protected Area2D body;
+
+			public override void _Ready()
+			{
+				base._Ready();
+
+				body = GetNode<Area2D>(bodyPath);
+			}
+		}
+	}
+}
--- src/node/NodeT.cs
+++ src/node/NodeT.cs
@@ -1,9 +1,4 @@
-﻿using Godot;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Com.Surbon.GDUtils.Node
+﻿namespace Com.Surbon.GDUtils.Node
 {
 	public static class NodeT
 	{
